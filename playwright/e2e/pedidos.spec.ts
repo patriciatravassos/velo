0ff99d3a9edaaa64fc0742ba702af498a80f8496
Patricia <travassos1985@gmail.com>
2026-01-22 18:56:53 +0000

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,27 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
+// AAA - Arrange, Act, Assert
 
 test('deve consultar pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+    // Arrange
+  await page.goto('http://localhost:5173/') 
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  
+  // Act
+  await page.getByTestId('search-order-id').fill('VLO-NULNMA') 
+  await page.getByTestId('search-order-button').click()
+
+  // Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-NULNMA')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 
 
-// Checkpoint
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
- 
-  // xpath: a[text()='Consultar Pedido']
-  // CSS Selector: a[href='/lookup']
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
-  await page.getByTestId('search-order-id').fill('VLO-NULNMA'); 
-
-  await page.getByTestId('search-order-button').click();
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-NULNMA');
-
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-
-
-});
+})
